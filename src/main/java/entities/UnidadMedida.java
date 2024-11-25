ff738f211fe9678b3341a81af413b9fe69b1799d
Brunoburgoa <brunocsir515@gmail.com>
2024-11-25 13:07:12 -0300

    Trabajo Finalizado

---
+++ src/main/java/entities/UnidadMedida.java
@@ -0,0 +1,18 @@
+package entities;
+
+
+import lombok.*;
+
+//@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+public class UnidadMedida {
+
+    private Long id;
+    private String denominacion;
+
+}
